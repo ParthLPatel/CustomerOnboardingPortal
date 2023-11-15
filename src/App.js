364e0a0b8a0a3833ddccf494e3ea08e45e91bc5a
paramvir_desai <paramvirjayrambhai.desai@cgi.com>
2023-11-15 11:26:23 -0500

    pushing photo id verification

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Header from './components/Header/Header';
-import UserTypePage from './components/UserTypePage/UserTypePage';
+import PhotoidVerification from './components/PhotoidVerification/PhotoidVerification';
+
 function App() {
   return (
     <div className="App">
 
       <Header />
 
+<<<<<<< Updated upstream
       <UserTypePage />
+=======
+      ----
+      <br />
+
+     
+
+      <PhotoidVerification />
+>>>>>>> Stashed changes
     </div>
   );
 }
