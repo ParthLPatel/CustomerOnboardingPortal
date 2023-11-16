3248ff74c00cddb687d0d5f7f7f3ceb0f348d79f
robin.yu <robin.yu@cgi.com>
2023-11-16 11:19:00 -0500

    FinancialInformationPage finished

--- src/components/FinancialInformation/FinancialInformation.jsx
+++ src/components/FinancialInformation/FinancialInformation.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form"
-
-const FinancialInformation = ()=>{
+import "./FinancialInformation.css"
+import { Link } from 'react-router-dom';
+const FinancialInformation = () => {
     const {
         register,
         handleSubmit,
@@ -11,7 +12,7 @@ const FinancialInformation = ()=>{
         formState: { errors },
     } = useForm()
 
-    const employmentStatusList=["Full Time Employment","Student","Retired","Self-Employment"];
+    const employmentStatusList = ["Full Time Employment", "Student", "Retired", "Self-Employment"];
     // const [employmentStatus,setEmploymentStatus] = useState("");
 
     const employmentStatus = watch("EmploymentStatus");
@@ -19,59 +20,82 @@ const FinancialInformation = ()=>{
     const onSubmit = (data) => {
         // Check if the default option is selected
         if (employmentStatus === "") {
-          setValue("EmploymentStatus", ""); // Reset the field value
-          // Set a custom error message for the EmploymentStatus field
-          setError("EmploymentStatus", {
-            type: "manual",
-            message: "Please select a valid employment status",
-          });
-          return;
+            setValue("EmploymentStatus", ""); // Reset the field value
+            // Set a custom error message for the EmploymentStatus field
+            setError("EmploymentStatus", {
+                type: "manual",
+                message: "Please select a valid employment status",
+            });
+            return;
         }
-    
+
         console.log(data);
-      };
+    };
 
 
     return (
         <div>
-                <div>Step 3 - Financial information</div>
-                <div>
-                    <div>We need to know some of your financial information</div>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className="container">
-                            <div className="row">
-                            <input className="col-12 form-control green-bottom-border" placeholder="Annual Income" {...register("AnnualIncome", { required: true })} />
-                            {errors.AnnualIncome && <span>This field is required</span>}
 
-                            <input className="col-12 form-control green-bottom-border" placeholder="Other household income (Optional)" {...register("AnnualIncome")} />
-                            
-                            <select class="form-select" aria-label="Select the employment status"{...register("EmploymentStatus",{ required: true })}>
-                                <option value="" selected disabled>Select Employment type</option>
-                                {
-                                    employmentStatusList.map(e=><option key={e} value={e}>{e}</option>)
-                                }
-                            </select>
-                            {errors.EmploymentStatus && <span>Please select an employment type</span>}
+            <div>
+
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="container">
+                        <div className="row subContainer">
+                            <div className="progress-step">Step 3 - Financial information</div>
+                            <p className="header_label">We need to know some of your financial information</p>
+                            <div className="input-div">
+                                <input className="col-12 form-control green-bottom-border" placeholder="Annual Income" {...register("AnnualIncome", { required: true })} />
+                                {errors.AnnualIncome && <span className="error-message">This field is required</span>}
 
                             </div>
-
+                            <div className="input-div">
+                                <input className="col-12 form-control green-bottom-border" placeholder="Other household income (Optional)" {...register("AnnualIncome")} />
+                            </div>
+                            <select class="form-select" aria-label="Select the employment status"{...register("EmploymentStatus", { required: true })}>
+                                <option value="" selected disabled>Select Employment type</option>
+                                {
+                                    employmentStatusList.map(e => <option key={e} value={e}>{e}</option>)
+                                }
+                            </select>
+                            {errors.EmploymentStatus && <span className="error-message">Please select an employment type</span>}
                             {
-                                employmentStatus=="Full Time Employment"?
-                                (<div>
-                                    <input className="col-12 form-control green-bottom-border" placeholder="Employer Name" {...register("EmployerName", { required: true })} />
-                                    {errors.EmployerName && <span>This field is required</span>}
-                                    <input className="col-12 form-control green-bottom-border" placeholder="Employer Industry" {...register("EmployerIndustry", { required: true })} />
-                                    {errors.EmployerIndustry && <span>This field is required</span>}
+                                employmentStatus == "Full Time Employment" ?
+                                    (<div className="employment-wrapper">
+                                        <div className="input-div">
+                                            <input className="col-12 form-control green-bottom-border" placeholder="Employer Name" {...register("EmployerName", { required: true })} />
+                                            {errors.EmployerName && <span className="error-message">This field is required</span>}
+                                        </div>
+                                        <div className="input-div"><input className="col-12 form-control green-bottom-border" placeholder="Employer Industry" {...register("EmployerIndustry", { required: true })} />
+                                            {errors.EmployerIndustry && <span className="error-message">This field is required</span>}
+                                        </div>
+                                    </div>) :
+                                    (employmentStatus == "Student" ? (<div className="employment-wrapper">
+                                        <div className="input-div">
+                                            <input className="col-12 form-control green-bottom-border" placeholder="Institution Name" {...register("InstitutionName", { required: true })} />
+                                            {errors.InstitutionName && <span className="error-message">This field is required</span>}
+                                        </div>
+                                        <div className="input-div"><input className="col-12 form-control green-bottom-border" type="date" placeholder="Graduation Date" {...register("GraduationDate", { required: true })} />
+                                            {errors.GraduationDate && <span className="error-message">This field is required</span>}
+                                        </div>
+                                    </div>) : (<div></div>))
 
-                                </div>):
-                                (employmentStatus=="Self-Employment"?(<div>Self-Employment</div>):(<div>Other</div>))
-                                
                             }
+
+                            <div className="btn-wrapper">
+                                <button type="submit" className="manulife-btn btn-orange text-decoration-none">
+                                    Submit
+                                </button>
+                                <Link to="/" className="manulife-btn btn-white text-decoration-none">
+                                    Back
+                                </Link>
+                            </div>
                         </div>
-                        <button className="btn btn-primary" >Back</button>
-                        <button className="btn btn-danger" type="submit">submit</button>
-                    </form>
-                </div>
+
+
+                    </div>
+
+                </form>
+            </div>
         </div>
     )
 
