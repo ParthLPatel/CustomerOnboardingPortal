8897524efb7ad130b14bd440b8345a950e80fe10
robin.yu <robin.yu@cgi.com>
2023-11-23 10:38:38 -0500

    Added page icon, added slider, added text field color

--- src/components/FinancialInformation/FinancialInformation.jsx
+++ src/components/FinancialInformation/FinancialInformation.jsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form"
+import { useForm, Controller } from "react-hook-form"
 import "./FinancialInformation.css"
 import { Link, useNavigate } from 'react-router-dom';
-
-
+import TextField from '@mui/material/TextField';
 import ProgressBar from "../ProgressBar/ProgressBar.js";
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import Select from '@mui/material/Select';
+import { Slider } from "@mui/material";
+
+import { ReactComponent as FinancialInformationPageIcon } from "./FinancialInformationPageIcon.svg"
+
+import { getIndustries, getOccupations } from "./joblist.js";
 
 
 const FinancialInformation = ({ financialInfoData, updateFinancialInfoData }) => {
@@ -15,11 +26,14 @@ const FinancialInformation = ({ financialInfoData, updateFinancialInfoData }) =>
         setValue,
         setError,
         getValues,
-        formState
+        formState,
+        clearErrors,
+        control
     } = useForm()
 
-    const employmentStatus = watch("employmentStatus");
+    // const employmentStatus = watch("employmentStatus");
     const navigate = useNavigate();
+    const [empStatus, setEmpStatus] = useState("");
 
     useEffect(() => {
         if (financialInfoData) {
@@ -34,12 +48,12 @@ const FinancialInformation = ({ financialInfoData, updateFinancialInfoData }) =>
     }, [financialInfoData, setValue]);
 
 
-    const employmentStatusList = ["Full Time Employment", "Student", "Retired", "Self-Employment"];
-
+    const employmentStatusList = ["Full Time Employment", "Student", "Retired", "unemployment"];
+    
 
     const onSubmit = (data) => {
         // Check if the default option is selected
-        if (employmentStatus === "") {
+        if (empStatus === "") {
             setValue("employmentStatus", ""); // Reset the field value
             // Set a custom error message for the EmploymentStatus field
             setError("employmentStatus", {
@@ -49,16 +63,16 @@ const FinancialInformation = ({ financialInfoData, updateFinancialInfoData }) =>
             return;
         }
         clearData(data);
-        updateFinancialInfoData({ ...data, employmentStatus });
+        updateFinancialInfoData({ ...data, employmentStatus: empStatus });
         navigate("/review-info");
     };
 
 
     const clearData = (data) => {
-        if (data.employmentStatus === "Full Time Employment") {
+        if (empStatus === "Full Time Employment") {
             data.institutionName = "";
             data.graduationDate = "";
-        } else if (data.employmentStatus === "Student") {
+        } else if (empStatus === "Student") {
             data.employerName = "";
             data.employerIndustry = "";
         } else {
@@ -73,9 +87,13 @@ const FinancialInformation = ({ financialInfoData, updateFinancialInfoData }) =>
         // Access form data when the "Cancel" button is clicked
         const data = getValues();
         clearData(data);
-        updateFinancialInfoData({ ...data, employmentStatus });
+        updateFinancialInfoData({ ...data, employmentStatus: empStatus });
     };
 
+    const handleEmpChange = (e) => {
+        clearErrors("employmentStatus");
+        setEmpStatus(e.target.value);
+    }
 
     return (
         <div>
@@ -83,66 +101,178 @@ const FinancialInformation = ({ financialInfoData, updateFinancialInfoData }) =>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="container">
                         <div className="progressBarContainer1">
-                                {/* <p className="progressBarLabel1">Step 4 - Financial information</p> */}
-                                <ProgressBar progress={5} /> {/* Pass the progress for this page */}
-                            </div>
+                            {/* <p className="progressBarLabel1">Step 4 - Financial information</p> */}
+                            <ProgressBar progress={4} /> {/* Pass the progress for this page */}
+                        </div>
                         <div className="row subContainer">
-                            
+                            <FinancialInformationPageIcon />
                             <p className="header_label">We need to know some of your financial information</p>
                             <div className="input-div ">
-                                <label className="labeldef">Annual Income</label>
-                                <div className="input-group">
-                                    <span className="input-group-text">$</span><input className="col-12 form-control green-bottom-border" placeholder="Annual Income" {...register("annualIncome", { required: true })} />
 
-                                </div>
-                                {(formState.errors.annualIncome) && <div className="error-message">This field is required</div>}
+                                <FormControl fullWidth sx={{ m: 1 }}>
+                                    <InputLabel htmlFor="personal-annual-income" color="success">Personal annual income</InputLabel>
+                                    <Controller
+                                        name="annualIncome"
+                                        control={control}
+                                        defaultValue={50000}
+                                        render={({ field }) => (
+                                            <>
+                                                <OutlinedInput
+                                                    id="personal-annual-income"
+                                                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                                    label="Personal annual income"
+                                                    {...field}
+                                                    {...register("annualIncome", { required: true })}
+                                                    color="success"
+                                                />
+                                                <FormHelperText sx={{ color: "crimson" }}>{formState.errors.annualIncome && "This field is required"}</FormHelperText>
+                                                <div className="slider-wrapper">
+                                                    <Slider
+                                                        value={field.value}
+                                                        onChange={(event, newValue) => setValue("annualIncome", newValue)}
+                                                        aria-labelledby="input-slider"
+                                                        valueLabelDisplay="auto"
+                                                        step={1000}
+                                                        min={50000}
+                                                        max={300000}
+                                                        sx={{
+                                                            color: '#09874E'
+                                                        }}
+                                                    />
+                                                    <div className="less-income">
+                                                        <span>Annual income less than $50,000? We recommend the <b>ManulifeMONEY+ Visa Platinum</b>, click <b><Link>HERE</Link></b> to apply!</span>
+                                                    </div>
+                                                </div>
+
+
+                                            </>
+                                        )}
+                                    />
+                                </FormControl>
+
+                            </div>
+                            <div>
 
                             </div>
                             <div className="input-div">
-                                <label className="labeldef">Other Household Annual Income</label>
-                                <div className="input-group">
-                                    <span className="input-group-text">$</span>
-                                    <input className="col-12 form-control green-bottom-border" placeholder="Other household income (Optional)" {...register("otherHouseholdIncome")} />
+                                <FormControl fullWidth sx={{ m: 1 }}>
+                                    <InputLabel htmlFor="household-annual-income" color="success">Other household annual income</InputLabel>
+                                    <Controller
+                                        name="householdIncome"
+                                        control={control}
+                                        defaultValue={0}
+                                        render={({ field }) => (
+                                            <>
+                                                <OutlinedInput
+                                                    id="household-annual-income"
+                                                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                                    label="Other household annual income"
+                                                    {...field}
+                                                    {...register("householdIncome")}
+                                                    color="success"
+                                                />
+                                                <Slider
+                                                    value={field.value}
+                                                    onChange={(event, newValue) => setValue("householdIncome", newValue)}
+                                                    aria-labelledby="input-slider"
+                                                    valueLabelDisplay="auto"
+                                                    step={1000}
+                                                    min={0}
+                                                    max={100000}
+                                                    sx={{
+                                                        color: '#09874E'
+                                                    }}
+                                                />
 
-                                </div>
+                                            </>
+                                        )}
+                                    />
+                                </FormControl>
+
                             </div>
                             <div className="input-div ">
-                            <label className="labeldef">Employment Status</label>
-                            <select className="form-select" defaultValue="" aria-label="Select the employment status" {...register("employmentStatus", { required: true })}>
-                                <option value="" disabled>Select Employment type</option>
-                                {
-                                    employmentStatusList.map(e => <option key={e} value={e}>{e}</option>)
-                                }
-                            </select>
-                            {formState.errors.employmentStatus && <span className="error-message">Please select an employment type</span>}
-                            
-                            
+                                <FormControl fullWidth sx={{ m: 1, minWidth: 120 }}>
+                                    <InputLabel id="employment-status" color="success">Employment status</InputLabel>
+                                    <Select
+                                        color="success"
+                                        labelId="employment-status"
+                                        id="employment-status"
+                                        value={empStatus}
+                                        label="Employment status"
+                                        onChange={handleEmpChange}
+
+                                    >
+                                        <MenuItem value="" disabled>
+                                            <em>None</em>
+                                        </MenuItem>
+                                        {
+                                            employmentStatusList.map(e => <MenuItem key={e} value={e}>{e}</MenuItem>)
+                                        }
+                                    </Select>
+                                </FormControl>
+                                {formState.errors.employmentStatus && <span className="error-message">Please select an employment type</span>}
+
+
                             </div>
 
                             {
-                                employmentStatus == "Full Time Employment" ?
+                                empStatus == "Full Time Employment" ?
                                     (<div className="employment-wrapper">
                                         <div className="input-div">
-                                            <label className="labeldef">Employer Name</label>
-                                            <input className="col-12 form-control green-bottom-border" placeholder="Employer Name" key="employerName" {...register("employerName", { required: true })} />
-                                            {formState.errors.employerName && <span className="error-message">This field is required</span>}
+                                            <FormControl fullWidth sx={{ m: 1 }}>
+                                                <InputLabel htmlFor="employer-name" color="success">Employer name</InputLabel>
+                                                <OutlinedInput
+                                                color="success"
+                                                    id="employer-name"
+                                                    label="Employer name"
+                                                    {...register("employerName", { required: true })}
+                                                />
+                                                <FormHelperText sx={{ color: "crimson" }}> {(formState.errors.employerName) && "This field is required"}</FormHelperText>
+                                            </FormControl>
                                         </div>
                                         <div className="input-div">
-                                            <label className="labeldef">Employer Industry</label>
-                                            <input className="col-12 form-control green-bottom-border" placeholder="Employer Industry" key="employerIndustry" {...register("employerIndustry", { required: true })} />
-                                            {formState.errors.employerIndustry && <span className="error-message">This field is required</span>}
+                                            <FormControl fullWidth sx={{ m: 1 }}>
+                                                <InputLabel htmlFor="employer-industry" color="success">Employer industry</InputLabel>
+                                                <OutlinedInput
+                                                color="success"
+                                                    id="employer-industry"
+                                                    label="Employer industry"
+                                                    {...register("employerIndustry", { required: true })}
+                                                />
+                                                <FormHelperText sx={{ color: "crimson" }}> {(formState.errors.employerIndustry) && "This field is required"}</FormHelperText>
+                                            </FormControl>
                                         </div>
                                     </div>) :
-                                    (employmentStatus == "Student" ? (<div className="employment-wrapper">
+                                    (empStatus == "Student" ? (<div className="employment-wrapper">
                                         <div className="input-div">
-                                            <label className="labeldef">Institution Name</label>
-                                            <input className="col-12 form-control green-bottom-border" placeholder="Institution Name" key="institutionName"  {...register("institutionName", { required: true })} />
-                                            {formState.errors.institutionName && <span className="error-message">This field is required</span>}
+                                            <FormControl fullWidth sx={{ m: 1 }}>
+                                                <InputLabel htmlFor="institution-name" color="success">Institution name</InputLabel>
+                                                <OutlinedInput
+                                                color="success"
+                                                    id="institution-name"
+                                                    label="Institution name"
+                                                    {...register("institutionName", { required: true })}
+                                                />
+                                                <FormHelperText sx={{ color: "crimson" }}> {(formState.errors.institutionName) && "This field is required"}</FormHelperText>
+                                            </FormControl>
                                         </div>
                                         <div className="input-div">
-                                            <label className="labeldef">Graduation Date</label>
-                                            <input className="col-12 form-control green-bottom-border" type="date" placeholder="Graduation Date" key="graduationDate"  {...register("graduationDate", { required: true })} />
-                                            {formState.errors.graduationDate && <span className="error-message">This field is required</span>}
+                                            <TextField
+                                            color="success"
+                                                id="graduation-date"
+                                                label="Date of Birth"
+                                                type="date"
+                                                variant="outlined"
+                                                InputLabelProps={{
+                                                    shrink: true,
+                                                    color:"success"
+                                                }}
+                                                fullWidth
+                                                sx={{ m: 1 }}
+                                                {...register("graduationDate", { required: true })}
+                                            />
+
+                                            <FormHelperText sx={{ color: "crimson" }}> {(formState.errors.graduationDate) && "This field is required"}</FormHelperText>
                                         </div>
                                     </div>) : (<div></div>))
 
---
+++ src/components/FinancialInformation/joblist.js
@@ -0,0 +1,56 @@
+const occupationMapping = {
+    "Agriculture/Forestry/Mining":[
+        "Administrator",
+        "Farmer (crop)",
+        "Farmer (livestock)",
+        "Fisheries Employee",
+        "Forestry Employee",
+        "Horticultruralist",
+        "Landscaper",
+        "Logger",
+        "Metals Processing Worker",
+        "Mining - Labourer",
+        "Pulp and Paer Processing Worker",
+        "Trapper/Hunter"
+    ],
+    "Arts & Entertainment & Media":[
+        "Actor",
+        "Agent",
+        "Amusement Park Employee",
+        "Arcade Employee",
+        "Artist",
+        "Athlete",
+        "Author",
+        "Broadcaster",
+        "Coach",
+        "Designer",
+        "Editor",
+        "Event Planner",
+        "Executive",
+        "Fitness Trainer",
+        "IT Professional",
+        "Journalist",
+        "Manager",
+        "Marketing/Sales"
+    ],
+    "Technology":[
+        "Administrator",
+        "Computer Prorammer/Developer",
+        "Engineer",
+        "Executive",
+        "IT Professional",
+        "Manager",
+        "Marketing/Sales",
+        "Web Designer"
+    ]
+}
+
+const getIndustries = ()=>{
+    return Object.keys(occupationMapping);
+}
+
+const getOccupation = industry=>{
+    return occupationMapping[industry];
+}
+
+module.exports = {getIndustries,getOccupation}
