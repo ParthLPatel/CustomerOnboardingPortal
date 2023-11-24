7b45734dc62d25d37f0659e6ade2aa94c1a7a7e5
parth.patel1 <parth.patel1@cgi.com>
2023-11-24 13:01:55 -0500

    added continueBtn-disabled functionality

--- src/components/VerifyIdentity2/VerifyIdentity2.js
+++ src/components/VerifyIdentity2/VerifyIdentity2.js
@@ -1,56 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react';
 import ProgressBar from "../ProgressBar/ProgressBar";
 import { Link } from 'react-router-dom';
-import './VerifyIdentity2.css'
+import './VerifyIdentity2.css';
 
 function VerifyIdentity2({ formData, updateFormData }) {
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
 
-    console.log(formData.IDtype)
+  const handleCheckboxChange = () => {
+    setIsCheckboxChecked(!isCheckboxChecked);
+  };
+
+  const handleContinueClick = () => {
+    // Check if the checkbox is checked before proceeding
+    if (isCheckboxChecked) {
+      // Continue with your logic
+      console.log('Continue button clicked');
+    } else {
+      // Optionally, you can show a message or perform some other action
+      console.log('Checkbox not checked. Please check before continuing.');
+    }
+  };
+
   return (
     <div className='container'>
+      <div className='subContainer'>
+        <div className="progressBarContainer1">
+          <ProgressBar progress={4} />
+        </div>
 
-        <div className='subContainer'>
+        <div className="section-header">Your ID looks good!</div>
+        <p className='subHeader'>Please check the details of your ID we captured</p>
 
-            <div className="progressBarContainer1">
-              {/* <p className="progressBarLabel1">Step 3.2 - Verify your identity</p> */}
-              <ProgressBar progress={4} /> {/* Pass the progress for this page */}
-            </div>
+        <div className="verification-details">
+          <p className='idContainer'>
+            <p className='dropdown my-1'>ID Type</p>
+            <p>{formData.IDtype}</p>
+          </p>
+          <p className='idContainer'>
+            <p className='dropdown my-1'>ID Number</p>
+            <p>123456</p>
+          </p>
+          <p className='idContainer'>
+            <p className='dropdown my-1'>Expiry Date</p>
+            <p>2030/05/21</p>
+          </p>
+        </div>
 
-            <div className="section-header">Your ID looks good!</div>
-            <p className='subHeader'>Please check the details of your ID we captured</p>
+        <div className="verification-checkbox">
+          <input
+            type="checkbox"
+            id="detailsVerified"
+            name="detailsVerified"
+            checked={isCheckboxChecked}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor='detailsVerified' className='checkboxLabel'>Details have been captured correctly</label>
+        </div>
 
-            <div className="verification-details">
-                {/* Display the captured details from step 1 */}
-                <p className='idContainer'>
-                    <p className='dropdown my-1'>ID Type</p> 
-                    <p>{formData.IDtype}</p>
-                </p>
-                <p className='idContainer'>
-                    <p className='dropdown my-1'>ID Number</p>
-                    <p>123456</p>
-                </p>
-                <p className='idContainer'>
-                    <p className='dropdown my-1'>Expiry Date</p> 
-                    <p>2030/05/21</p>
-                </p>
-            </div>
-
-            <div className="verification-checkbox">
-                <input type="checkbox" id="detailsVerified" name="detailsVerified"/>
-                <label for='detailsVerified' className='checkboxLabel'>Details have been captured correctly</label>
-            </div>
-            <div className="btn-wrapper my-4 px-0">
-                <Link to="/verify-identity" className="manulife-btn btn-white text-decoration-none">
-                    Back
-                </Link>
-                <Link to="/financial-info" className="manulife-btn btn-orange text-decoration-none">
-                Continue
-                </Link>
-            </div>   
-        </div> 
-
+        <div className="btn-wrapper my-4 px-0">
+          <Link to="/verify-identity" className="manulife-btn btn-white text-decoration-none">
+            Back
+          </Link>
+          {isCheckboxChecked ? (
+            <Link to="/financial-info" className="manulife-btn btn-orange text-decoration-none ">
+              Continue
+            </Link>
+          ) : (
+            <span className="manulife-btn btn-orange btn-orange-lighter">Continue</span>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default VerifyIdentity2
+export default VerifyIdentity2;
