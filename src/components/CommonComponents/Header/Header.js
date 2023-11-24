667a341350e16b6921b3cd05a0ae51e0a2a8bc9c
parth.patel1 <parth.patel1@cgi.com>
2023-11-24 14:35:58 -0500

    added FR-style & froze header

--- src/components/CommonComponents/Header/Header.js
+++ src/components/CommonComponents/Header/Header.js
@@ -1,31 +1,67 @@
-import React from 'react'
-import './Header.css'
+// import React from 'react'
+// import './Header.css'
 
-import manulife_logo from './manulife_logo.svg'
+// import manulife_logo from './manulife_logo.svg'
+
+// function Header() {
+//   return (
+//         <div className='header-container'>
+//             <div className='row'>
+//                 <div className='col-3 box1'>
+//                     <img src={manulife_logo} alt="Your SVG" className="manulifeLogo"/>
+//                 </div>
+//                 <div className='col-9 box2'>
+//                     {/* text and fr language */}
+//                         <p className="para">
+//                            <span className="para_text_1">Manulife</span>
+//                            <span className="para_text_2"> Bank</span>
+//                         </p>
+                     
+//                         <div className="fr_language ml-auto">
+//                             <div className='fr_language_inner'>
+//                                 <p className="fr_text1">FR</p>
+//                             </div>
+//                        </div>
+                    
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Header
+
+
+// Header.js
+
+import React from 'react';
+import './Header.css';
+
+import manulife_logo from './manulife_logo.svg';
 
 function Header() {
   return (
-            <div className='row'>
-                <div className='col-3 box1'>
-                    <img src={manulife_logo} alt="Your SVG" className="manulifeLogo"/>
-                </div>
-                <div className='col-9 box2'>
-                    {/* text and fr language */}
-                        <p className="para">
-                           <span className="para_text_1">Manulife</span>
-                           <span className="para_text_2"> Bank</span>
-                        </p>
-                     
-                        {/* <div className="fr_language ml-auto">
-                            <div className='fr_language_inner'>
-                                <p className="fr_text1">FR</p>
-                            </div>
-                       </div> */}
-                    
-                </div>
+    <div className='header-container'>
+      <div className='row'>
+        <div className='col-3 box1'>
+          <img src={manulife_logo} alt="Your SVG" className="manulifeLogo"/>
+        </div>
+        <div className='col-9 box2'>
+          {/* text */}
+          <p className="para">
+            <span className="para_text_1">Manulife</span>
+            <span className="para_text_2"> Bank</span>
+          </p>
+          {/* FR language */}
+          <div className="fr_language ml-auto">
+            <div className='fr_language_inner'>
+              <p className="fr_text1">FR</p>
             </div>
-    )
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
-
+export default Header;
