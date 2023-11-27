c1a5d4f0dffe474f2827596cfaeffc7d5e835cef
paramvir_desai <paramvirjayrambhai.desai@cgi.com>
2023-11-26 19:33:19 -0500

    usertype get started page

--- src/components/UserTypePage/UserTypePage.js
+++ src/components/UserTypePage/UserTypePage.js
@@ -1,41 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserTypePage.css";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Manulife_green_Logo from "../../assets/Manulife_Logo1.svg";
 
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
-</style>
+function UserTypePage() {
+    const [showThankYou, setShowThankYou] = useState(false);
+    const navigate = useNavigate();
 
-function UserTypePage(){
+    const handleNoClick = () => {
+        setShowThankYou(true);
+    };
+
+    const handleApplyNowClick = () => {
+        // Navigate to the next page or perform other actions
+        navigate('/create-profile');
+    };
+
+    const handleYesClick = () => {
+        // Perform any actions needed for "Yes"
+        // ...
+
+        // Hide the "Thank you" message and "Apply Now" button
+        setShowThankYou(false);
+    };
+
     return (
         <div className='container'>
             <div className="subContainer">
-                <div className="section-header">Great Choice!</div>
-                <div className="usertype-disclaimer">
-                    <p>A recap text of product description and highlight, make the client be more confident in their decision. </p>
-                    <p>Applying for a Personal Credit Card can take between 5-15 minutes. You’ll want to have these things ready:</p>
-                    <div>
-                        <ul>
-                            <li>A government issued photo ID</li>
-                            <li>Your Social Insurance Number (SIN)</li>
-                            <li>Your mobile device or computer camera to confirm your identity</li>
-                        </ul>
-                    </div>
-                    <p>You must be 18 years of age or older to apply</p>
+                <div className="section-header">
+                    <img src={Manulife_green_Logo} alt="Your SVG" className="ManulifeLogo"/>
                 </div>
-                <div className="usertype-q">Do you already have an account and bank with us online?</div>
-                <div className="btn-wrapper">
-                    <Link to="###" className="manulife-btn btn-orange text-decoration-none">
-                        Yes
-                    </Link>
-                    <Link to="create-profile" className="manulife-btn btn-white text-decoration-none">
-                        No
-                    </Link>
+                <div className="usertype-q" style={{ width: "100%", marginTop: '45px' }}>
+                    Do you already have an account and bank with us online?
+                </div>
 
-                </div>
+                <Stack direction="column" spacing={2} style={{ width: "100%", marginTop: '45px' }}>
+                    <Button
+                        variant="outlined"
+                        style={{
+                            color: "#434559",
+                            borderColor: "#434559",
+                            paddingTop: '10px',
+                            paddingBottom: '10px',
+                            paddingRight: '30px',
+                            paddingLeft: '30px',
+                            justifyContent: 'flex-start',
+                            textAlign: 'left',
+                            fontSize: '20px',
+                            fontWeight: '600',
+                            textTransform: 'none'
+                        }}
+                        onClick={handleYesClick}
+                    >
+                        Yes
+                    </Button>
+                    {!showThankYou ? (
+                        <Button
+                            variant="outlined"
+                            style={{
+                                color: "#434559",
+                                borderColor: "#434559",
+                                paddingTop: '10px',
+                                paddingBottom: '10px',
+                                paddingRight: '30px',
+                                paddingLeft: '30px',
+                                justifyContent: 'flex-start',
+                                textAlign: 'left',
+                                fontSize: '20px',
+                                fontWeight: '600',
+                                textTransform: 'none'
+                            }}
+                            onClick={handleNoClick}
+                        >
+                            No
+                        </Button>
+                    ) : (
+                        <div
+                            style={{
+                                width: "100%",
+                                height: "200px",
+                                padding: '20px 30px 20px 30px',
+                                border: '2px solid #09874E',
+                                borderRadius: '4px',
+                                gap: '20px',
+                                marginTop: '20px'
+                            }}
+                        >
+                            <div className="no">No</div>
+                            <div style={{ fontSize: '15px', fontWeight: '400', marginTop: '20px', lineHeight: '18px' }}>
+                                Thank you for choosing us! <br></br > Let’s get started setting you up as a new customer.
+                            </div>
+                            <Button
+                                variant="outlined"
+                                style={{
+                                    color: "#ED6453",
+                                    borderWidth:'2px',
+                                    borderColor: "#ED6453",
+                                    fontSize: '16px',
+                                    fontWeight: '600',
+                                    textTransform: 'none',
+                                    marginTop: '20px',
+                                    width: '256px',
+                                    height:'52px'
+                                }}
+                                onClick={handleApplyNowClick}
+                            >
+                                Apply Now
+                            </Button>
+                        </div>
+                    )}
+                </Stack>
             </div>
         </div>
-    )
+    );
 }
 
-export default UserTypePage
+export default UserTypePage;
